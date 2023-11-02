8f6ee2561b9d80eaad67fddf1b937b01ec674ec1
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2023-11-02 20:16:29 +0100

    combine css

--- apps/table-website/src/environments/environment.prod.ts
+++ apps/table-website/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   env: "prod",
-  version: '02.11.2023 19:24',
-  commitHash: '405d36a'
+  version: '02.11.2023 20:15',
+  commitHash: '782ed7c'
 };
