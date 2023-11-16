65d38fc37f23d72260b701932bfc8d2398edec8e
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2023-11-16 17:32:27 +0100

    solidtable fixed

--- apps/table-website/src/environments/environment.prod.ts
+++ apps/table-website/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   env: "prod",
-  version: '16.11.2023 12:12',
-  commitHash: '6f6363c'
+  version: '16.11.2023 17:31',
+  commitHash: '895f51e'
 };
