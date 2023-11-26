e66bf37112897f95777702bfbfec6cce2b810810
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2023-11-26 16:57:49 +0100

    instanceof workaround

--- libs/table/src/lib/table/instanceof-workaround.ts
+++ libs/table/src/lib/table/instanceof-workaround.ts
@@ -5,18 +5,18 @@
  */
 
 export function isTreeRow(row: any): boolean {
-  return row['type'] === 'TreeRow';
+  return row && row['type'] === 'TreeRow';
 }
 
 
 
 export function isAreaModelTree(model: any): boolean {
-  return model['type'] === 'AreaModelTree';
+  return model && model['type'] === 'AreaModelTree';
 }
 
 
 export function isCheckboxColumnDef(o: any): boolean {
-  return o['type'] === 'CheckboxColumnDef';
+  return o && o['type'] === 'CheckboxColumnDef';
 }
 
 
