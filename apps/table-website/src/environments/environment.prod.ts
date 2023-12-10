f5d7b9c19d7f69108dfe5f2c92c50b504723eef4
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2023-12-10 20:31:47 +0100

    token

--- apps/table-website/src/environments/environment.prod.ts
+++ apps/table-website/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   env: "prod",
-  version: '10.12.2023 19:00',
-  commitHash: '789e20b'
+  version: '10.12.2023 20:30',
+  commitHash: '7c54266'
 };
