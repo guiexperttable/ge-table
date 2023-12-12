f639a55c9479bf740730818f5fa43fe194c4bdb0
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2023-12-12 17:09:01 +0100

    16.0.7

--- apps/table-website/src/environments/environment.prod.ts
+++ apps/table-website/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   env: "prod",
-  version: '10.12.2023 20:30',
-  commitHash: '7c54266'
+  version: '12.12.2023 17:07',
+  commitHash: 'f5d7b9c'
 };
