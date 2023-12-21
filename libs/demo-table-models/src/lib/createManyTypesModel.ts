8a2b454eb38a690a0295513ef8de1b5acf9be653
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2023-12-21 09:48:21 +0100

    createManyTypesModelAndOptions

---
+++ libs/demo-table-models/src/lib/createManyTypesModel.ts
@@ -0,0 +1,192 @@
+import {
+  CheckboxBooleanPropertyCellRenderer,
+  ColumnDef,
+  ColumnDefIf,
+  DateToIntlDDMMYYYYCellRenderer,
+  ProgressBarCellRenderer,
+  px100,
+  px120,
+  px140, px200,
+  px60,
+  px90,
+  SelectionModel,
+  SelectionModelIf,
+  StarRatingCellRenderer,
+  TableFactory,
+  TableModelAndOptions,
+  TableModelAndOptionsIf,
+  TableOptions,
+  TrueFn
+} from '@guiexpert/table';
+import manyTypesData from '../demodata/many-types';
+import { ManyTypesIf } from '../model/mana-types.if';
+
+
+function createTableRows(rowCount: number = 75): ManyTypesIf[] {
+  return manyTypesData.slice(0, rowCount - 1);
+}
+
+function createColumnDefs(): ColumnDefIf[] {
+  return [
+      ColumnDef.create({
+      property: 'company',
+      headerLabel: 'Company',
+      width: px120,
+      bodyClasses: ['ge-table-text-align-left'],
+      headerClasses: ['ge-table-text-align-left'],
+      sortable: TrueFn
+    }),
+    ColumnDef.create({
+      property: 'country',
+      headerLabel: 'Country',
+      width: px140,
+      bodyClasses: ['ge-table-text-align-left'],
+      headerClasses: ['ge-table-text-align-left'],
+      sortable: TrueFn
+    }),
+    ColumnDef.create({
+      property: 'product',
+      headerLabel: 'Name',
+      width: px120,
+      bodyClasses: ['ge-table-text-align-left'],
+      headerClasses: ['ge-table-text-align-left'],
+      sortable: TrueFn
+    }),
+    ColumnDef.create({
+      property: 'birthday',
+      headerLabel: 'Birth Date',
+      width: px100,
+      bodyClasses: ['ge-table-text-align-center'],
+      sortable: TrueFn
+    }),
+    ColumnDef.create({
+      property: 'sellDate',
+      headerLabel: 'Sell date',
+      width: px100,
+      bodyRenderer: new DateToIntlDDMMYYYYCellRenderer(),
+      bodyClasses: ['ge-table-text-align-center'],
+      sortable: TrueFn
+    }),
+    ColumnDef.create({
+      property: 'lastVisit',
+      headerLabel: 'Last Visit',
+      width: px100,
+      bodyClasses: ['ge-table-text-align-center'],
+      sortable: TrueFn
+    }),
+    ColumnDef.create({
+      property: 'time',
+      headerLabel: 'Time',
+      width: px100,
+      bodyClasses: ['ge-table-text-align-center'],
+      sortable: TrueFn
+    }),
+    ColumnDef.create({
+      property: 'id', headerLabel: 'Order ID', width: px120, bodyClasses: ['ge-table-text-align-center'], sortable: TrueFn
+    }),
+    ColumnDef.create({
+      property: 'inStock',
+      headerLabel: 'In stock',
+      width: px90,
+      bodyRenderer: new CheckboxBooleanPropertyCellRenderer('inStock', true, true),
+      bodyClasses: ['ge-table-text-align-center'],
+      sortable: TrueFn
+    }),
+    ColumnDef.create({
+      property: 'quantity',
+      headerLabel: 'Qty',
+      width: px60,
+      bodyClasses: ['ge-table-text-align-right'],
+      headerClasses: ['ge-table-text-align-right'],
+      sortable: TrueFn
+    }),
+    ColumnDef.create({
+      property: 'progress',
+      headerLabel: 'Progress',
+      width: px100,
+      bodyRenderer: new ProgressBarCellRenderer('progress', 5, true),
+      bodyClasses: ['ge-table-text-align-left'],
+      sortable: TrueFn
+    }),
+    ColumnDef.create({
+      property: 'rating',
+      headerLabel: 'Rating',
+      width: px100,
+      bodyRenderer: new StarRatingCellRenderer('rating'),
+      bodyClasses: ['ge-table-text-align-left'],
+      sortable: TrueFn
+    }),
+
+    ColumnDef.create({
+      property: 'price',
+      headerLabel: 'Price',
+      width: px100,
+      bodyClasses: ['ge-table-text-align-right'],
+      sortable: TrueFn
+    }),
+    ColumnDef.create({
+      property: 'active',
+      headerLabel: 'Active',
+      width: px100,
+      bodyClasses: ['ge-table-text-align-right'],
+      sortable: TrueFn
+    }),
+    ColumnDef.create({
+      property: 'currency',
+      headerLabel: 'CCY',
+      width: px100,
+      bodyClasses: ['ge-table-text-align-right'],
+      sortable: TrueFn
+    }),
+    ColumnDef.create({
+      property: 'average',
+      headerLabel: 'Average',
+      width: px100,
+      bodyClasses: ['ge-table-text-align-right'],
+      sortable: TrueFn
+    }),
+    ColumnDef.create({
+      property: 'f1',
+      headerLabel: 'F1',
+      width: px100,
+      bodyClasses: ['ge-table-text-align-right'],
+      sortable: TrueFn
+    }),
+    ColumnDef.create({
+      property: 'f2',
+      headerLabel: 'F2',
+      width: px100,
+      bodyClasses: ['ge-table-text-align-right'],
+      sortable: TrueFn
+    }),
+    ColumnDef.create({
+      property: 'lorem',
+      headerLabel: 'Lorem',
+      width: px200,
+      sortable: TrueFn
+    })
+
+  ];
+}
+
+function createManyTypesTableOptions(selectionModel: SelectionModelIf): TableOptions {
+  return {
+    ...new TableOptions(), hoverColumnVisible: false, defaultRowHeights: {
+      header: 24, body: 24, footer: 0
+    }, getSelectionModel: () => selectionModel
+  };
+}
+
+export function createManyTypesModelAndOptions(
+  selectionModel: SelectionModelIf = new SelectionModel('row', 'multi'),
+  rowCount: number = 75
+): TableModelAndOptionsIf {
+  const tableOptions = createManyTypesTableOptions(selectionModel);
+  rowCount = Math.max(10, Math.min(75, rowCount));
+  const rows: ManyTypesIf[] = createTableRows(rowCount);
+  const columnDefs: ColumnDefIf[] = createColumnDefs();
+  return new TableModelAndOptions(TableFactory.createTableModel({
+    rows, columnDefs, tableOptions
+  }), tableOptions);
+}
+
---
+++ libs/demo-table-models/src/model/mana-types.if.ts
@@ -0,0 +1,22 @@
+
+export interface ManyTypesIf {
+  id: string
+  company: string
+  country: string
+  product: string
+  birthday: string
+  sellDate: string
+  lastVisit: string
+  time: string
+  inStock: boolean
+  quantity: number
+  progress: number
+  rating: number
+  price: number
+  active: boolean
+  currency: string
+  lorem: string
+  average: string
+  f1: number
+  f2: number
+}
