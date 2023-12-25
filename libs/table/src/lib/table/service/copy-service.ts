cc6f194362a4a021cb4909374e05dc96a6e05885
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2023-12-25 10:35:10 +0100

    wip: COPY_2_CLIPBOARD

--- libs/table/src/lib/table/service/copy-service.ts
+++ libs/table/src/lib/table/service/copy-service.ts
@@ -2,28 +2,61 @@ import { TableModelIf } from '../data/tablemodel/table-model.if';
 import { SelectionModelIf } from '../selection/selection-model.if';
 import { FocusModelIf } from '../focus/focus-model.if';
 import { CopyServiceIf } from './copy-service.if';
+import { CellRange } from '../data/common/cell-range';
 
 
 export class CopyService implements CopyServiceIf {
 
-    public static columnSeparatorChar = '\t';
-    public static rowSeparatorChar = '\n';
+  public static columnSeparatorChar = '\t';
+  public static rowSeparatorChar = '\n';
+
+
+  private mergeRanges(ranges: CellRange[]): CellRange | undefined {
+    let mergedRange: CellRange | undefined;
+    for (const range of ranges) {
+      if (!mergedRange) {
+        mergedRange = new CellRange(range.r1, range.c1, range.r2, range.c2);
+      } else {
+        mergedRange.r1 = Math.min(mergedRange.r1, range.r1);
+        mergedRange.c1 = Math.min(mergedRange.c1, range.c1);
+        mergedRange.r2 = Math.max(mergedRange.r2, range.r2);
+        mergedRange.c2 = Math.max(mergedRange.c2, range.c2);
+      }
+    }
+    return mergedRange;
+  }
 
   createContent(
     tableModel: TableModelIf,
     selectionModel: SelectionModelIf,
     focusModel: FocusModelIf
   ): Promise<string> {
-    //TODO hier gehts weiter
-    const cs = CopyService.columnSeparatorChar;
-    const rs = CopyService.rowSeparatorChar;
-    console.info(cs, rs);
-    console.info(selectionModel);
-    console.info(selectionModel);
-    console.info(tableModel);
-    console.info(focusModel);
-    return new Promise<string>((resolve, _reject) => {
-      resolve('');
+
+    return new Promise<string>((resolve, reject) => {
+
+      if (selectionModel?.hasSelection()) {
+        const range: CellRange | undefined = this.mergeRanges(selectionModel.getRanges());
+        if (range) {
+          const content: Array<string> = [];
+          for (let rowIndex = range.r1; rowIndex <= range.r2; rowIndex++) {
+            const rowData: string[] = [];
+            for (let columnIndex = range.c1; columnIndex <= range.c2; columnIndex++) {
+              const cellValue = tableModel.getBodyModel().getValueAt(rowIndex, columnIndex);
+              rowData.push(cellValue);
+            }
+            content.push(rowData.join(CopyService.columnSeparatorChar));
+          }
+          return resolve(content.join(CopyService.rowSeparatorChar));
+        }
+      }
+
+      if (focusModel) {
+        const [r, c] = focusModel.getFocus();
+        const content =  String(tableModel.getBodyModel().getValueAt(r, c));
+        return resolve(content);
+      }
+
+      reject('Cannot copy, neither selection nor focus defined.');
     });
   }
 
