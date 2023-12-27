eb24fb98d157c84ccac870c7fe83e8871743c37c
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2023-12-27 15:45:21 +0100

    #wip: toggleHeaderGroup

--- apps/table-website/src/environments/environment.prod.ts
+++ apps/table-website/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   env: "prod",
-  version: '26.12.2023 22:07',
-  commitHash: '4b96d04'
+  version: '27.12.2023 15:44',
+  commitHash: 'd404a2d'
 };
