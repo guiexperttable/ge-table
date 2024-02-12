256aae1186516cf29253085c958b09e0f875061c
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2024-02-12 20:37:45 +0100

    Refactor imports, update package scripts, and cleanup .gitignore
    Refactored import statements in 'positive-and-negative-number-renderer.ts' file for improved readability and organization. Updated 'package.json' in 'preact-table' by adding a 'build' script. Also cleaned up '.gitignore' by removing a redundant '/dist/' entry.

--- libs/table/src/lib/table/renderer/positive-and-negative-number-renderer.ts
+++ libs/table/src/lib/table/renderer/positive-and-negative-number-renderer.ts
@@ -1,4 +1,8 @@
-import { AreaIdent, AreaModelIf, CellRendererIf, DomServiceIf, RendererCleanupFnType } from "@guiexpert/table";
+import {CellRendererIf} from "./cell-render.if";
+import {AreaIdent} from "../data/tablemodel/area-ident.type";
+import {AreaModelIf} from "../data/tablemodel/areamodel/area-model.if";
+import {DomServiceIf} from "../service/dom-service.if";
+import {RendererCleanupFnType} from "./renderer-cleanup-fn.type";
 
 
 export class PositiveAndNegativeNumberRenderer implements CellRendererIf {
