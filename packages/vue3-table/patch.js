902076546d29761f0e73df25357530ba2619a0d2
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2024-02-14 20:34:32 +0100

    Add automatic patch versioning scripts
    Implemented scripts to automatically patch versions of multiple packages. Each package now includes a 'patch.js' file to increment its version number, and the main package.json includes scripts to run these patch scripts for individual or all packages.

---
+++ packages/vue3-table/patch.js
@@ -0,0 +1,17 @@
+// import fs from 'fs';
+const fs = require('fs');
+
+const packageJsonPath = 'package.json';
+
+try {
+  const pack = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+  const currentVersion = pack.version;
+  const [major, minor, patch] = currentVersion.split('.').map(Number);
+  const newPatchVersion = `${major}.${minor}.${patch + 1}`;
+  pack.version = newPatchVersion;
+  fs.writeFileSync(packageJsonPath, JSON.stringify(pack, null, 2));
+  console.log(`${pack.name} -> version patched to ${newPatchVersion}.`);
+
+} catch (error) {
+  console.error('Error:', error);
+}
