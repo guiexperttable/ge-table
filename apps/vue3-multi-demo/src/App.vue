b89147e6f957d0d990b16cf469d6883833fed47e
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2024-02-16 19:52:32 +0100

    Add routing to Vue3 multi-demo app
    Implemented Vue3 routing on the vue3-multi-demo application. It includes setting up the vue-router, adding "Home" and "About" pages, and making related changes in the main.ts and App.vue files. Dependencies in the package.json file have been updated to incorporate the vue-router.

--- apps/vue3-multi-demo/src/App.vue
+++ apps/vue3-multi-demo/src/App.vue
@@ -1,3 +1,14 @@
+
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view />
+</template>
+
+
+<!--
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 </script>
@@ -28,3 +39,4 @@ import HelloWorld from './components/HelloWorld.vue'
   filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
+-->
