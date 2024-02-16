5d422364b023860d7bd4df5f4fff29ab05395d8a
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2024-02-16 22:28:46 +0100

    Add Quasar framework to vue3-multi-demo
    Integrated Quasar framework into vue3-multi-demo application. This includes the creation of a new file for Quasar Sass variables, adding Quasar as a dependency in the package.json file and configuring the Quasar plugin in the vite.config.ts file. Several development-related dependencies and Sass configuration updates were also made.

--- apps/vue3-multi-demo/src/App.vue
+++ apps/vue3-multi-demo/src/App.vue
@@ -1,11 +1,51 @@
 
 <template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header reveal elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <!-- drawer content -->
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+const leftDrawerOpen = ref(false);
+
+const toggleLeftDrawer = () => {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+};
+</script>
+
+<!--template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
-</template>
+</template-->
 
 
 <!--
--- apps/vue3-multi-demo/src/main.ts
+++ apps/vue3-multi-demo/src/main.ts
@@ -2,7 +2,18 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { Quasar } from 'quasar'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
 
 createApp(App)
   .use(router)
+  .use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+  })
   .mount('#app')
