790c6c5baf9b27965484c6c75d45a1a5152188a7
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2024-02-16 23:09:09 +0100

    Updated logos, style, and app data in vue3-multi-demo
    Removed the previous vite.svg and vue.svg logos, added a new favicon. Changed the primary app title, and updated colour definitions in the Quasar Sass variables file. Also incorporated additional styling measures in the main style.css file.

--- apps/vue3-multi-demo/src/App.vue
+++ apps/vue3-multi-demo/src/App.vue
@@ -7,10 +7,7 @@
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
         <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          Title
+          <b>GUI</b>Expert Table Demos
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
@@ -39,44 +36,3 @@ const toggleLeftDrawer = () => {
 };
 </script>
 
-<!--template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
-</template-->
-
-
-<!--
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--->
